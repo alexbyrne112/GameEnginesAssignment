9030240812d479d71c5f068456d76b0ab0d53412
Alex Byrne <c15492952@mydit.ie>
2018-12-17 20:17:36 +0000

    added to targetment move

--- Assets/TargetMove.cs
+++ Assets/TargetMove.cs
@@ -9,6 +9,7 @@ public class TargetMove : MonoBehaviour {
     List<Vector3> destinations = new List<Vector3>();
     public float speed = 5;
     public Transform target;
+    public int current = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -19,4 +20,16 @@ public class TargetMove : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private void Awake()
+    {
+        float ThetaInc = (Mathf.PI * 2) / numDestinations;
+        for(int i = 0; i < numDestinations; i++)
+        {
+            float theta = i * ThetaInc;
+            Vector3 pos = new Vector3(Mathf.Sin(theta) * radius, 0, Mathf.Cos(theta) * radius);
+            pos = transform.TransformPoint(pos);
+            destinations.Add(pos);
+        }
+    }
 }
