7a6705b75feab0815d370c82346175521371ee63
Alex Byrne <c15492952@mydit.ie>
2018-12-17 20:12:38 +0000

    target movemnet

---
+++ Assets/TargetMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMove : MonoBehaviour {
+
+    public float radius = 30;
+    public int numDestinations = 8;
+    List<Vector3> destinations = new List<Vector3>();
+    public float speed = 5;
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
