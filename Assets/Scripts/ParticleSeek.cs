af8caabe81fa384e9021c4866227344816a3d8a8
Alex Byrne <c15492952@mydit.ie>
2018-12-17 19:12:08 +0000

    added if statement for world spacee issue of PS

--- Assets/Scripts/ParticleSeek.cs
+++ Assets/Scripts/ParticleSeek.cs
@@ -16,6 +16,27 @@ public class ParticleSeek : MonoBehaviour {
 	void Update () {
         ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.particleCount];
         ps.GetParticles(particles);
-        
-	}
+        for (int i = 0; i < particles.Length; i++)
+        {
+            ParticleSystem.Particle p = particles[i];//reference of particle
+            Vector3 particleWorldPos;
+            if(ps.main.simulationSpace == ParticleSystemSimulationSpace.Local)//for different positions relitive to the World/Local/Custom Space
+            {
+                particleWorldPos = transform.TransformPoint(p.position);
+            }
+            else if(ps.main.simulationSpace == ParticleSystemSimulationSpace.Custom)
+            {
+                particleWorldPos = ps.main.customSimulationSpace.transform.TransformPoint(p.position);
+            }
+            else
+            {
+                particleWorldPos = p.position;
+            }
+            Vector3 targetDirection = (target.position - particleWorldPos).normalized; //normalised to stop it adding extra force to the particle
+            Vector3 seekforce = targetDirection * force * Time.deltaTime;
+            p.velocity += seekforce;
+            particles[i] = p;//update back into array;
+        }
+        ps.SetParticles(particles, particles.Length);//add all particles back into Particle System
+    }
 }
