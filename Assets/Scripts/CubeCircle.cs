fd79436789c4b6a615041b1ed026a7f0abcb9e22
Alex Byrne <c15492952@mydit.ie>
2018-12-17 15:24:11 +0000

    added to Music Analiser

---
+++ Assets/Scripts/CubeCircle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCircle : MonoBehaviour {
+    public float scale = 10;
+    List<GameObject> cyls = new List<GameObject>();
+    public float radius = 100;
+
+	// Use this for initialization
+	void Start () {
+        CreateCubeCircle();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CreateCubeCircle()
+    {
+
+    }
+}
--- Assets/Scripts/MusicAnalyser.cs
+++ Assets/Scripts/MusicAnalyser.cs
@@ -1,25 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [RequireComponent(typeof(AudioSource))]
 public class MusicAnalyser : MonoBehaviour {
 
     AudioSource audio;
-    public float[] samples = new float[512];
+    public static int frameSize = 512;
+    public static float[] samples = new float[512];
+    public static float[] bands;
+    private AudioClip clip;
+    private AudioMixerGroup amgMaster;
+    public float binWidth;
+    public float sampleRate;
 
-	// Use this for initialization
-	void Start () {
+    private void Awake()
+    {
         audio = GetComponent<AudioSource>();
-	}
+        samples = new float[frameSize];
+        bands = new float[(int)Mathf.Log(frameSize, 2)];
+
+
+        audio.clip = clip;
+        audio.outputAudioMixerGroup = amgMaster;
+        audio.Play();
+    }
+
+    // Use this for initialization
+    void Start () {
+        sampleRate = AudioSettings.outputSampleRate;
+        binWidth = AudioSettings.outputSampleRate / 2 / frameSize;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        GetSpectrum();
-	}
-
-    void GetSpectrum()
-    {
         audio.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+        GetBands();
+    }
+
+    void GetBands()
+    {
+   
     }
 }
