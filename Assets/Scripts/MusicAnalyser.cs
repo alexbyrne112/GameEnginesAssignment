1e35d651c297d3e98876077e9e7ea332bba80e21
Alex Byrne <c15492952@mydit.ie>
2018-12-17 14:21:11 +0000

    Song and analiser script

--- Assets/Scripts/MusicAnalyser.cs
+++ Assets/Scripts/MusicAnalyser.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class MusicAnalyser : MonoBehaviour {
+
+    AudioSource audio;
+    public float[] samples = new float[512];
 
 	// Use this for initialization
 	void Start () {
-		
+        audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        GetSpectrum();
 	}
+
+    void GetSpectrum()
+    {
+        audio.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
 }
