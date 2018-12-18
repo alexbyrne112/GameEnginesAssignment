c7a07dd5bbf51a56509c20e5427c4ebef94a7eb4
Alex Byrne <c15492952@mydit.ie>
2018-12-18 15:11:06 +0000

    start terrain

---
+++ Assets/TerraineGen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TerraineGen : MonoBehaviour {
+    public int width = 256;
+    public int height = 256;//length
+    public int depth = 20;
+
+    public float scale = 20f;
+
+    private void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);//terrain data = newly generated terrain based off current terrain data
+    }
+
+    TerrainData GenerateTerrain()
+    {
+
+    }
+}
+
