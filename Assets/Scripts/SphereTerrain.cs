b25f33c5d46a3fd5447563eb8177ac09e3624787
Alex Byrne <c15492952@mydit.ie>
2018-12-19 18:33:23 +0000

    vertices for sphere

--- Assets/Scripts/SphereTerrain.cs
+++ Assets/Scripts/SphereTerrain.cs
@@ -20,6 +20,37 @@ public class SphereTerrain {
         AxisB = Vector3.Cross(locUp, AxisA);
     }
 
+    public void ConstructMesh()
+    {
+        Vector3[] vertices = new Vector3[resolution*resolution];
+        int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
+        int triIndex = 0;
+        for(int i = 0; i< resolution; i++)
+        {
+            for(int j =0; j < resolution; j ++)
+            {
+                int vertex = i + j * resolution;
+                Vector2 percent = new Vector2(i, j) / (resolution - 1);//used to define where each vertex should be on the face
+                Vector3 PointOnCube = locUp + (percent.x - 0.5f) * 2 * AxisA + (percent.y - 0.5f) * 2 * AxisB;
+                vertices[vertex] = PointOnCube;//vertex on the cube
+
+                if(j!= resolution-1 && i!= resolution-1)
+                {
+                    //creating first triangle
+                    triangles[triIndex] = vertex;
+                    triangles[triIndex+1] = vertex +resolution +1;
+                    triangles[triIndex+2] = vertex +resolution;
+
+                    //second triangle
+                    triangles[triIndex+3] = vertex;
+                    triangles[triIndex+4] = vertex + 1;
+                    triangles[triIndex+5] = vertex + resolution + 1;
+                    triIndex += 6;//becaise 6 vertices used
+                }
+            }
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
 		
