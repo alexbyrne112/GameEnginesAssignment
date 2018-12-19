5a9a3ff6c9016d99eb318a22bd3ce1d7eabd631b
Alex Byrne <c15492952@mydit.ie>
2018-12-19 18:16:52 +0000

    sphere

--- Assets/Scripts/CubeCircle.cs
+++ Assets/Scripts/CubeCircle.cs
@@ -14,7 +14,7 @@ public class CubeCircle : MonoBehaviour {
     // Use this for initialization
     void Start () {
         CreateCubeCircle();
-        CreateSphere();
+        //CreateSphere();
     }
 
     void CreateCubeCircle()
---
+++ Assets/Scripts/SphereTerrain.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereTerrain {
+
+    Mesh mesh;
+    int resolution;
+    Vector3 locUp;
+    Vector3 AxisA;
+    Vector3 AxisB;
+
+    public SphereTerrain(Mesh mesh, int resolution, Vector3 locUp)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.locUp = locUp;
+
+        AxisA = new Vector3(locUp.y, locUp.z, locUp.x);
+        AxisB = Vector3.Cross(locUp, AxisA);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
